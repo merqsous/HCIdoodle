26a2ea6f0dcbcdd87d446983e0d8d58894437d97
merqsous <moatherqsous@gmail.com>
2022-11-24 02:26:43 -0500

    new
    new

---
+++ DrawingView.java
@@ -0,0 +1,74 @@
+package com.example.HCIdoodle;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
+
+public class DrawingView extends View {
+    private ArrayList<Path> paths = new ArrayList<>();
+    private ArrayList<Integer> colors = new ArrayList<>();
+    private int currentColor = 0xFF000000;
+    private ArrayList<Integer> widths = new ArrayList<>();
+    private int currentWidth = 6;
+
+
+
+    public DrawingView(Context context) {
+        super(context);
+    }
+
+    public DrawingView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DrawingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+    public void addPath(Path path) {
+        paths.add(path);
+        colors.add(currentColor);
+        widths.add(currentWidth);
+    }
+
+    public Path getLastPath() {
+        if (paths.size() > 0) {
+            return paths.get(paths.size() - 1);
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int i = 0;
+        for (Path path : paths) {
+
+                Paint paint = new Paint();
+                paint.setColor(colors.get(i));
+                paint.setStyle(Paint.Style.STROKE);
+                paint.setStrokeWidth(widths.get(i));
+                canvas.drawPath(path, paint);
+                i++;
+        }
+    }
+    public void setCurrentColor(int color) {
+
+        currentColor = color;
+    }
+    public void setCurrentWidth(int width) {
+        currentWidth = (width + 1) * 4;
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.HCIdoodle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    DrawingView drawingView;
+    SeekBar seekBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawingView = findViewById(R.id.canvas);
+        drawingView.setOnTouchListener(new TouchListener());
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                drawingView.setCurrentWidth(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void setColor(View view) {
+        ColorDrawable buttonColor = (ColorDrawable) view.getBackground();
+        drawingView.setCurrentColor(buttonColor.getColor());
+    }
+}
